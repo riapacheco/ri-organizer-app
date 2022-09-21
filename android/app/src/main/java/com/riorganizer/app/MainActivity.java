e89f1614e6ef9d2c9bd53b9fcea2cd4771995562
Ria Pacheco <ria.pacheco@coldboretechnology.com>
2022-09-20 19:59:53 -0600

    Capacitor for ios and android

---
+++ android/app/src/main/java/com/riorganizer/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.riorganizer.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.riorganizer.app',
+  appName: 'ri-organizer-app',
+  webDir: 'dist/ri-organizer-app',
+  bundledWebRuntime: false
+};
+
+export default config;
